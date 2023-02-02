f009a5960f42e8dcd689e0a7fa7f93156d28f232
joaovitorsb97 <jvs.bastos@gmail.com>
2023-02-02 19:24:37 -0300

    Custom validation error response

--- backend/src/main/java/com/joaovitorsb/dscatalog/controllers/exception/ExceptionHandlerController.java
+++ backend/src/main/java/com/joaovitorsb/dscatalog/controllers/exception/ExceptionHandlerController.java
@@ -4,6 +4,8 @@ import com.joaovitorsb.dscatalog.services.exceptions.DatabaseException;
 import com.joaovitorsb.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,12 +18,34 @@ public class ExceptionHandlerController{
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest request){
-        StandardError standardError = new StandardError(Instant.now(), HttpStatus.NOT_FOUND.value(), "Resource not found", exception.getMessage(), request.getRequestURI());
+        StandardError standardError = new StandardError(Instant.now(),
+                                                        HttpStatus.NOT_FOUND.value(),
+                                                        "Resource not found",
+                                                        exception.getMessage(),
+                                                        request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
     }
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> databaseError(DatabaseException exception, HttpServletRequest request){
-        StandardError standardError = new StandardError(Instant.now(), HttpStatus.BAD_REQUEST.value(), "Database error", exception.getMessage(), request.getRequestURI());
+        StandardError standardError = new StandardError(Instant.now(),
+                                                        HttpStatus.BAD_REQUEST.value(),
+                                                        "Database error",
+                                                        exception.getMessage(),
+                                                        request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validationError(MethodArgumentNotValidException exception, HttpServletRequest request){
+        ValidationError validationError = new ValidationError(Instant.now(),
+                                                        HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                                                        "Validation Error",
+                                                        exception.getMessage(),
+                                                        request.getRequestURI());
+
+        for(FieldError fieldError : exception.getBindingResult().getFieldErrors()){
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(validationError);
     }
 }
---
+++ backend/src/main/java/com/joaovitorsb/dscatalog/controllers/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.joaovitorsb.dscatalog.controllers.exception;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError() {
+        super();
+    }
+
+    public ValidationError(Instant timeStamp, Integer status, String error, String message, String path) {
+        super(timeStamp, status, error, message, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String fieldMessage){
+        errors.add(new FieldMessage(fieldName, fieldMessage));
+    }
+}
