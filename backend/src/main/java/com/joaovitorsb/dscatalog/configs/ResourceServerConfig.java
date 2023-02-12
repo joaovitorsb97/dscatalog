d0807172c90a5c67a157cd744e6594133b81279a
joaovitorsb97 <jvs.bastos@gmail.com>
2023-02-12 17:01:29 -0300

    H2 security configuration (permiting all to access at H2 console)

--- backend/src/main/java/com/joaovitorsb/dscatalog/configs/ResourceServerConfig.java
+++ backend/src/main/java/com/joaovitorsb/dscatalog/configs/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.joaovitorsb.dscatalog.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,14 +11,19 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = { "/oauth/token" };
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
@@ -30,6 +36,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //H2
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
         .antMatchers(PUBLIC).permitAll() //Everybody can access vector PUBLIC routes
         .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll() //Permit only method GET for all at vector OPERATOR_OR_ADMIN routes
